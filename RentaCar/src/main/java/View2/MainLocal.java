a0f7d1b64b2e076bf06fc10f5a071ec1857e0cbe
KevinSalbra <kevinsalazar74.ksb@gmail.com>
2023-06-01 23:43:21 -0600

    N1.5

---
+++ RentaCar/src/main/java/View2/MainLocal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View2;
+
+/**
+ *
+ * @author Kevin Salazar
+ */
+public class MainLocal {
+    
+    public static void main(String[] args) {
+        MenuCamion menu = new MenuCamion();
+        menu.setVisible(true);
+    }
+}
